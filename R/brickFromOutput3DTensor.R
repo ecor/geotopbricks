d6ab5e0b485acdf1c6113c4d8d92545bf299bcc9
ecor <emanuele.cordano@gmail.com>
2014-05-22 15:01:16 +0200

    Geotopbricks 1.3.5.4c

--- R/brickFromOutput3DTensor.R
+++ R/brickFromOutput3DTensor.R
@@ -142,7 +142,8 @@ NULL
 #' # a 2D map: 
 #' x_e <- "SnowDepthMapFile"
 #' # Not Run: uncomment the following line
-#'  m <- rasterFromOutput2DMap(x_e,when=when,wpath=wpath,timestep="OutputSnowMaps",tz=tz)
+#'  m <- rasterFromOutput2DMap(x_e,when=when,wpath=wpath,timestep="OutputSnowMaps",tz=tz,use.read.raster.from.url=TRUE)
+#' ## NOTE: set use.read.raster.from.url=FALSE (default) if the "wpath" directorty is in the local file system.
 #' # Not Run: uncomment the following line
 #' # plot(m)
 #'
